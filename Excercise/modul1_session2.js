4f0ac14325fccd56963149f89d55b7f9d1ad1c83
nicoPrasetiawan <nicoprasetiawan28@gmail.com>
2024-05-02 23:04:19 +0700

    completing remaining excercise in session 2

--- Excercise/modul1_session2.js
+++ Excercise/modul1_session2.js
@@ -33,17 +33,70 @@ console.log(separator);
 
 //Write a code to check whether the number is prime number or not
 // bilangan yang hanya bisa dibagi 1 dan bilangan itu sendiri... punya 2 pembagi habis
+console.log("check whether the number is prime number or not");
+function isPrime(n) {
+    let divider = 0;
 
+    for(let i = 1; i <= n; i++) {
+        if(n % i === 0) {
+            divider++
+        }
+    }
+
+    let result = divider === 2 ? `${n} is a prime number.` : `${n} is NOT a prime number`;
+    console.log(result);
+}
+isPrime(6);
 
 
 console.log(separator);
 
 //Write a code to find the sum of the numbers 1 to N
 console.log("Sum of the numbers 1 to N");
-function sumToN(n) {
-    let sum = 0;
 
-    for(let i = 0; i < n; i++) {
-        sum += n;
+function sumToN(n) {
+    let increaser = 0;
+
+    for(let i = 1; i <= n; i++) {
+        increaser += i;
     }
+    return `${n} -> ${increaser}`;
 }
+console.log(sumToN(5));
+
+
+console.log(separator);
+
+// Write a code to find factorial of a number
+console.log("find factorial of a number");
+function factorianNum(n) {
+    let result = 1;
+
+    for(let i = n; i > 0; i--) {
+        result *= i;
+    }
+
+    return `${n}! -> ${result}`
+}
+console.log(factorianNum(6));
+
+console.log(separator);
+
+//Write a code to print the first N fibonacci numbers
+console.log("print the first N fibonacci numbers");
+
+function fibb(n) {
+    let a = 0;
+    let b = 1;
+    let nextNum = 0;
+
+    for(let i = 1; i < n; i++) {
+        nextNum = a + b;
+        a = b;
+        b = nextNum;
+    }
+
+    return `${n} -> ${nextNum}`;
+}
+
+console.log(fibb(15));
